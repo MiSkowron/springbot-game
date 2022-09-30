8e19abd12a87c87e8cd7dcfdbe6a0c8811814d2b
Mike <mskowron4@yahoo.com>
2022-09-29 20:49:51 -0700

    more routes and handlebar updates

--- Develop/controllers/api/userRoutes.js
+++ Develop/controllers/api/userRoutes.js
@@ -20,7 +20,7 @@ router.post('/', async (req, res) => {
 //user login
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.email } });
+    const userData = await User.findOne({ where: { email: req.body.email } });
 
     if (!userData) {
       res
